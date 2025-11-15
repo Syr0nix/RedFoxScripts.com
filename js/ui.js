685246b6d998c466ddf5a6806799ff52c804124a
RedFox <110576731+Syr0nix@users.noreply.github.com>
2025-11-15 13:16:17 -0500

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,23 +1,14 @@
 // js/obfuscator.js
-// Base structure for your future real obfuscator engine
-
 console.log("RedFox Obfuscator Loaded.");
 
-// Create global object so ui.js can call it:
 window.RedFoxObfuscator = {
     obfuscate: function (code, opts) {
-        
-        // ------------------------------
-        //  TEMPORARY SIMPLE OBFUSCATOR
-        //  (Just to make UI WORK NOW)
-        // ------------------------------
 
         let hex = "";
         for (let i = 0; i < code.length; i++) {
             hex += code.charCodeAt(i).toString(16).padStart(2, "0");
         }
 
-        // Fake layers
         let layers = 
             (opts.variableRename ? 1 : 0) +
             (opts.stringEncrypt ? 1 : 0) +
@@ -27,7 +18,6 @@ window.RedFoxObfuscator = {
             (opts.antiDebug ? 1 : 0) +
             (opts.antiTamper ? 1 : 0);
 
-        // Output stub
         let wrapped = `
 -- RedFox Obfuscator (TEMP ENGINE)
 local data = "${hex}"
@@ -38,7 +28,7 @@ for i = 1, #data, 2 do
     out = out .. string.char(byte)
 end
 
-loadstring(out)()
+return loadstring(out)()
 `;
 
         return {
