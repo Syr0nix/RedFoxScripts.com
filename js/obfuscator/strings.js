e77d2f3c63cc99e71b09cf240269216c3ec13532
RedFox <110576731+Syr0nix@users.noreply.github.com>
2025-11-15 13:28:48 -0500

    Update strings.js

--- js/obfuscator/strings.js
+++ js/obfuscator/strings.js
@@ -1,1 +1,18 @@
+// js/obfuscator/strings.js
 
+export function encryptStrings(code) {
+    return code.replace(/"(.-)"/g, function(_, str) {
+
+        // XOR key (random per run)
+        let key = Math.floor(Math.random() * 200) + 30;
+
+        // convert to bytes
+        let bytes = [];
+        for (let i = 0; i < str.length; i++) {
+            bytes.push(str.charCodeAt(i) ^ key);
+        }
+
+        // encode as lua
+        return `(_RF_DEC("${bytes.join(",")}",${key}))`;
+    });
+}
